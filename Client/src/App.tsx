e65da3ec289f97cbb2529b41abbb95987b445b51
iqura9 <pozitiv_ON@ukr.net>
2023-02-28 15:44:17 +0200

    asdsd

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,30 +1,34 @@
-import React, {useEffect, useState} from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 import HeaderSelector from "./Components/HeaderSelector";
 import ResultComponent from "./Components/ResultComponent";
-import {io} from "socket.io-client";
+import { io } from "socket.io-client";
 
-const URL = 'http://localhost:1040';
+//const URL = 'http://localhost:1040';
+const URL = "https://aoc-project.onrender.com/";
 export const socket = io(URL);
 
 function App() {
-    const [value, setValue] = useState<{ value:string, time:any }>({value:'',time:0});
-    useEffect(() => {
-        socket.on("connect", () => {});
-        socket.on("result", (value:string) => {
-            const time = Date.now();
-            setValue({value,time});
-            //console.log(`time end: ${time}`)
-        });
-    }, []);
-    return (
-        <div className="App">
-            <div className="App__Container">
-                <HeaderSelector/>
-                <ResultComponent value={value.value} time={value.time} />
-            </div>
-        </div>
-    );
+  const [value, setValue] = useState<{ value: string; time: any }>({
+    value: "",
+    time: 0,
+  });
+  useEffect(() => {
+    socket.on("connect", () => {});
+    socket.on("result", (value: string) => {
+      const time = Date.now();
+      setValue({ value, time });
+      //console.log(`time end: ${time}`)
+    });
+  }, []);
+  return (
+    <div className="App">
+      <div className="App__Container">
+        <HeaderSelector />
+        <ResultComponent value={value.value} time={value.time} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
